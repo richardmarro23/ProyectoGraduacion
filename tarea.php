0918634f3d8bb58c2068399b44877572a1d1db19
richardmarro23 <52941574+richardmarro23@users.noreply.github.com>
2019-07-16 23:29:27 -0600

    Add files via upload

--- tarea.php
+++ tarea.php
@@ -6,25 +6,22 @@
 			$db_name="bmddqndirbgg56hcvqbh";
 		    $info=mysqli_connect($host,$user,$password,$db_name)or die("no se pudo conectar a la base de datos");
 	
-					    $registros = '{"usuarios":[';
+					    $registros = "[";
 			            $cosulta="select nombre, correo, contra from usuario";
 			            $result = mysqli_query ($info, $cosulta);
 	                    while($fila=mysqli_fetch_array($result)){
-			    if ($registros != '{"usuarios":[') {
-				$registros .= ',';
+			    if ($registros != "[") {
+				$registros .= ",";
 			}
-			$registros .= '{"nombre":"'.$fila["nombre"].'",';
-			$registros .= '"correo":"'.$fila["correo"].'",';
-			$registros .= '"contra":"'.$fila["contra"].'"}';
+			$registros .= '{"nombre": "'.$fila["nombre"].'",';
+			$registros .= '"correo": "'.$fila["correo"].'",';
+			$registros .= '"contra": "'.$fila["contra"].'"}';
 		}
-		$registros .= ']}';
+		$registros .= "]";
 		echo $registros;
 		            
 			            
 	                ?>
 			
-        </div>  			
-	</body>
-</html>
 
 
